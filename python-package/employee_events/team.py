a3800c3475a03b4c6e26fa24e5fb4c374afae283
Golam Sarowar Jahan Rifat <gsjrifat@gmail.com>
2025-03-04 11:31:59 +0100

    1st commit

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,46 +1,44 @@
 # Import the QueryBase class
-# YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies for sql execution
-#### YOUR CODE HERE
+import sqlite3
+import pandas as pd
+
 
 # Create a subclass of QueryBase
 # called  `Team`
-#### YOUR CODE HERE
-
+class Team(QueryBase):
     # Set the class attribute `name`
     # to the string "team"
-    #### YOUR CODE HERE
-
+    name = "team"
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-        
-        # Query 5
-        # Write an SQL query that selects
-        # the team_name and team_id columns
-        # from the team table for all teams
-        # in the database
-        #### YOUR CODE HERE
-    
+    def names(self):
+        # Query 5: Select team_name and team_id from the team table
+        sql_query = """
+            SELECT
+                team_name,
+                team_id
+            FROM team
+        """
+        return self.query(sql_query=sql_query)
 
     # Define a `username` method
     # that receives an ID argument
     # This method should return
     # a list of tuples from an sql execution
-    #### YOUR CODE HERE
-
-        # Query 6
-        # Write an SQL query
-        # that selects the team_name column
-        # Use f-string formatting and a WHERE filter
-        # to only return the team name related to
-        # the ID argument
-        #### YOUR CODE HERE
-
+    def username(self, id):
+        # Query 6: Select team_name for the team with the specified ID
+        sql_query = f"""
+        SELECT team_name
+            FROM {self.name}
+            WHERE team_id = {id}
+        """
+        return self.query(sql_query=sql_query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -49,18 +47,19 @@
     # so when it is called, a pandas dataframe
     # is returns containing the execution of
     # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
-            SELECT positive_events, negative_events FROM (
-                    SELECT employee_id
-                         , SUM(positive_events) positive_events
-                         , SUM(negative_events) negative_events
+        query = f"""
+                SELECT positive_events, negative_events
+                FROM (
+                    SELECT
+                        employee_id,
+                        SUM(positive_events) AS positive_events,
+                        SUM(negative_events) AS negative_events
                     FROM {self.name}
                     JOIN employee_events
-                        USING({self.name}_id)
+                    USING ({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                     GROUP BY employee_id
-                   )
-                """
+                )
+            """
+        return self.pandas_query(query)
